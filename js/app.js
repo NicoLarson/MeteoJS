78a47a9546d65a28f2942f8f00454695bcd92be2
Yang <nicolas.yang.1@gmail.com>
2020-10-06 13:46:12 +0200

    V1.0

---
+++ js/app.js
@@ -0,0 +1,65 @@
+
+let targetNow = document.querySelector('main section:nth-child(1) p')
+let targetIn3Hours = document.querySelector('main section:nth-child(2) p')
+let targetTomorrow = document.querySelector('main section:nth-child(3) p')
+let request = new XMLHttpRequest()
+
+let city = document.querySelector('main section:first-child input')
+let click = document.querySelector('main section:first-child button')
+
+click.addEventListener('click', () => {
+    let requestURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city.value}&appid=46360059671f55734266b0751f95671e`
+
+    request.onreadystatechange = function () {
+        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+            let response = JSON.parse(this.responseText)
+
+            let display = (target, t) => {
+                let iconURL = `https://openweathermap.org/img/wn/${response.list[t].weather[0].icon}.png`
+                let tempCelsius = kelvinToCelsius(response.list[t].main.temp)
+
+                target.innerHTML = `<img src="${iconURL}" alt=""><br>
+                                 <i class="fas fa-thermometer-half"></i> ${tempCelsius}°C<br>
+                                 <i class="fas fa-wind"></i> ${response.list[t].wind.speed} m/s`
+
+            }
+            display(targetNow, 0)
+            display(targetIn3Hours, 1)
+            display(targetTomorrow, 2)
+        }
+    }
+
+
+    request.open('GET', requestURL)
+    request.send()
+
+})
+request.onreadystatechange = function () {
+    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+        let response = JSON.parse(this.responseText)
+
+        let display = (target, t) => {
+            let iconURL = `https://openweathermap.org/img/wn/${response.list[t].weather[0].icon}.png`
+            let tempCelsius = kelvinToCelsius(response.list[t].main.temp)
+
+            target.innerHTML += `<img src="${iconURL}" alt=""><br>
+                             <i class="fas fa-thermometer-half"></i> ${tempCelsius}°C<br>
+                             <i class="fas fa-wind"></i> ${response.list[t].wind.speed} m/s`
+
+        }
+        display(targetNow, 0)
+        display(targetIn3Hours, 1)
+        display(targetTomorrow, 2)
+    }
+}
+
+
+
+
+let kelvinToCelsius = (kelvin) => {
+    let celsius = kelvin - 273.15
+    return Number.parseFloat(celsius).toFixed(2)
+}
+
+request.open('GET', requestURL)
+request.send()
